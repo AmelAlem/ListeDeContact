f84ef5616245e4a35f55af82b5e2eb68abc5c9ab
Amel ALEM <mlle.amel@hotmail.fr>
2019-10-21 14:29:23 +0200

    liste de contact

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Contact from './Components/Contacts';
+import ContactList from './Components/ContactList';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+<div className="contact">
 
+    <ContactList />
+    <Contact
+    
+    avatar='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvVeAVHoPrPXS1uqCfFGyvhUW4wvmX8sMbuY2su3yaOxLZN2J0NQ&s'
+    name='sophie'
+    />
+    <Contact
+    
+    avatar='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmtpqrtfXbzRCIplQpVz6pvqGeW3J5uICyYWOg9uBWejQYKkHt&s://encrypted-tbn0.gstathttps://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeVgH7bpNcAr25Yzj0eZ6VafC78XU3ywb5-T2xWm7Aa8gw3PVi&sic.com/images?q=tbn:ANd9GcRvVeAVHoPrPXS1uqCfFGyvhUW4wvmX8sMbuY2su3yaOxLZN2J0NQ&s'
+    name='laurie'
+    status
+    />
+     <Contact
+    
+    avatar='https://upload.wikimedia.org/wikipedia/commons/f/f7/Dame_Edna_%286959716988%29.jpg'
+    name='nadine'
+    status
+    
+    />
+
+  </div>
+  );
+  }
 export default App;
+
+
