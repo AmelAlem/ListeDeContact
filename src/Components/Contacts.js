98a05161df73a94af3ac254e3722a5c069085c30
Amel ALEM <mlle.amel@hotmail.fr>
2019-10-28 15:15:09 +0100

    state

--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -2,20 +2,46 @@ import React from 'react';
 import './Contact.css'
 
 
-function Contact(props) {
+class Contacts extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+         status: false,
+         };
+    }
+    render() {
+        const statusOnline = (
+            <div className="status">
+            <div className="status-online"/>
+            <div className="status-text">Online</div>
+            </div>
+        );
+        const statusOffline = (
+            <div className="status">
+            <div className="status-offline"/>
+            <div className="status-text">Offline</div>
+            </div>
+        );
 
-return (
+    return (
     <div className="Contact">
-        <img className="avatar" src={props.avatar} alt={props.name} />
+        <img className="avatar" src={this.props.avatar} alt={this.props.name} />
         <div>
-          <h4 className="name" >{props.name}</h4>
-          <div className="status">
-          <span className={props.status ? 'status-online' : 'status-offline'} ></span>
-          {props.status ? 'online' : 'offline'}
-          </div>
-          </div>
+           <h4 className="name" >{this.props.name}</h4>
+           <div className="status">
+                <span className={this.state.status ? 'statusOnline' : 'statusOffline'} 
+                onClick={event => {
+                const newStatus = !this.state.status;
+                this.setState({ status: newStatus });
+                }}
+                >
+                    {this.state.status ? statusOnline : statusOffline}
+                </span>
+            </div>
+        </div>
         
-      </div>
-)
+    </div>
+        );
+    }
 }
-export default Contact;
+export default Contacts;
